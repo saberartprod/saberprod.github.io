81dfdc58e353b3278abf3522c6e3e940a36c9728
saberartprod <saberartprod@gmail.com>
2016-06-21 06:52:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Saber PROD</title>
+   <link rel="shortcut icon" href="ico.jpg" type="image/jpg">
+   <link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+   <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300&subset=latin,greek,greek-ext,latin-ext,cyrillic-ext,cyrillic,vietnamese' rel='stylesheet' type='text/css'>
+  <style>
+  h1{
+	font-family: 'Abel', sans-serif;
+  }
+  h2{
+	 font-family: 'Open Sans Condensed', sans-serif;
+  }
+   body {
+    background: #c7b39b url(background.jpg); 
+    color: #fff; 
+   }
+    A {
+    text-decoration: none; /* Убирает подчеркивание для ссылок */
+	 color: white;
+   } 
+   A:hover { 
+    text-decoration: underline; /* Добавляем подчеркивание при наведении курсора на ссылку */
+    color: white; /* Ссылка красного цвета */
+   } 
+   .page-header {
+    border-bottom: 1px solid #ccc;
+}
+  </style>
+ </head>
+ <body>
+ <center><h1>Saber PROD<h1></center>
+<div class = "page-header"></div>
+<center><h1><a href="about.php">About</a> - <a href="work.php">Work</a> - <a href="blog.php">Blog</a></h1></center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<h2><center><p>Coming soon</p></h2></center>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</br>
+<div class = "page-header"></div>
+<center><h1>Contact</h1></center>
+<center>
+
+<a href="https://new.vk.com/thesaberart"><img src="vk.png"></a>
+<a href="https://plus.google.com/u/4/105920886836534930684/posts"><img src="g+.png"></a>
+<a href="https://twitter.com/saber_prod"><img src="tw.png"></a>
+<a href="https://www.youtube.com/channel/UCgNDDytSAWrodrZG7ugYcow"><img src="you.png"></a>
+<center><h1>saberartprod@gmail.com</h1></center>
+</center>
+ </body>
